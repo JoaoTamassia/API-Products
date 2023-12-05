64431826d00b484ea4f095a6c18426ceeb4285c5
João Tamassia <joaotamassia@alunos.utfpr.edu.br>
2023-12-05 15:39:32 -0300

    API DO CURSO ( YOUTUBE )

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,52 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+    private String name;
+    private BigDecimal price;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
